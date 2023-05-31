73361277533d8c33ed7aa6bf1a933bd0bd4ee55a
Crowdbotics <team@crowdbotics.com>
2023-05-31 19:35:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "e-ticketing-app-41132",
+  url: "https://e-ticketing-app-41132.botics.co",
+  api: "https://e-ticketing-app-41132.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
